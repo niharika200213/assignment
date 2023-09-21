ef7274e9bad77a66e2492610846cf31639b2742d
niharika rastogi <rastoginiha2002@gmail.com>
2023-09-21 21:09:36 +0530

    first commit

--- dist/errors/notFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const customAPIError_1 = require("./customAPIError");
-const notFound = (req, res, next) => {
-    const notFoundError = (0, customAPIError_1.createCustomError)(`Cannot find ${req.originalUrl} at this server`, 404);
-    return next(notFoundError);
-};
-exports.default = notFound;
-//# sourceMappingURL=notFound.js.map
